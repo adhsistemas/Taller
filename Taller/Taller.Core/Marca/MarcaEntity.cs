4c0c481807ac691228f0223c52f62253b080738e
GitDevForceOne <98342063+GitDevForceOne@users.noreply.github.com>
2022-08-25 10:41:49 -0600

    Creacion de core
    Se creo modelo Marca

---
+++ Taller/Taller.Core/Marca/MarcaEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace Taller.Entidades
+{
+    public class MarcaEntity
+    {
+        public int IdMarca { get; set; }
+
+        public string Nombre { get; set; }
+
+        public bool EstaActivo { get; set; }
+    }
+}
